f24b32dc8125869a90088ca4cbba299149dbcd2f
brian <brianarguello358@gmail.com>
2024-09-15 20:15:58 -0400

    base de ahorcado

--- main.py
+++ main.py
@@ -6,10 +6,27 @@ intentos = 0
 
 with open("diccionario.txt", "r") as archivo:
     palabra = archivo.readlines()
-    #eturn palabra
-print(random.choice(palabra).strip())
+
+adivinar = random.choice(palabra).strip()
 
 while intentos < 6:
+    barra = " _ "
+    frase = barra * len(adivinar)
+    print(frase)
+    letra = str(input("Agrega una letra: "))
+    if letra in adivinar:
+        print("Bien")
+    else:
+        intentos = intentos + 1
+        print("Fallaste")
+
+
+
+
+
+"""while intentos < 6:
     intentos = intentos + 1
     print(intentos)
+"""
+
 
