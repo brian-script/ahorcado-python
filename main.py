cbec5f1021d141757b4d976ea5d8b8fad08832c2
brian <brianarguello358@gmail.com>
2024-09-18 10:40:45 -0400

    ascii

--- main.py
+++ main.py
@@ -1,5 +1,79 @@
 import random
 
+
+def ascii(intentos):
+    if intentos == 0:
+        print("""
+                  +------+
+                  |      |
+                         |
+                         |
+                         |
+                         |
+                 =========
+              """)
+    elif intentos == 1:
+        print("""
+                  +------+
+                  |      |
+                  O      |
+                         |
+                         |
+                         |
+                 =========
+              """)
+    elif intentos == 2:
+        print("""
+                  +------+
+                  |      |
+                  O      |
+                  |      |
+                         |
+                         |
+                 =========
+              """)
+    elif intentos == 3:
+        print("""
+                  +------+
+                  |      |
+                  O      |
+                 /|      |
+                         |
+                         |
+                 =========
+              """)
+    elif intentos == 4:
+        print("""
+                  +------+
+                  |      |
+                  O      |
+                 /|\     |
+                         |
+                         |
+                 =========
+              """)
+    elif intentos == 5:
+        print("""
+                  +------+
+                  |      |
+                  O      |
+                 /|\     |
+                 /       |
+                         |
+                 =========
+              """)
+    elif intentos == 6:
+        print("""
+                  +------+
+                  |      |
+                  O      |
+                 /|\     |
+                 / \     |
+                         |
+                 =========
+              """)
+
+
 print("****Juego Ahorcado****\n")
 
 intentos = 0
@@ -9,24 +83,24 @@ with open("diccionario.txt", "r") as archivo:
 
 adivinar = random.choice(palabra).strip()
 
-while intentos < 6:
-    barra = " _ "
-    frase = barra * len(adivinar)
-    print(frase)
+progreso = ["_"] * len(adivinar)
+
+ascii(intentos)
+while intentos < 6 and "_" in progreso:
+
+    print(" ".join(progreso))
     letra = str(input("Agrega una letra: "))
+
     if letra in adivinar:
         print("Bien")
     else:
         intentos = intentos + 1
         print("Fallaste")
 
+    ascii(intentos)
 
+if intentos == 6:
+    print(f"la frase era {adivinar}")
 
-
-
-"""while intentos < 6:
-    intentos = intentos + 1
-    print(intentos)
-"""
 
 
